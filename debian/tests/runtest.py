1e7235108a1f270340e1145ca32c0ecb0d223eca
David Steele <dsteele@gmail.com>
2023-08-21 18:42:57 -0400

    Fix README autopkgtest

--- debian/tests/runtest.py
+++ debian/tests/runtest.py
@@ -4,7 +4,7 @@ import asyncio
 import aiofiles
 
 async def test_aiofiles():
-    async with aiofiles.open('README.rst', 'r') as fp:
+    async with aiofiles.open('README.md', 'r') as fp:
         contents = await fp.read()
 
     assert('aiofiles' in contents)
